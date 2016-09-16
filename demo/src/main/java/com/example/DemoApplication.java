3a0ef128a561cb00edb5945de573fa114aa5c02b
mohamedboui <bouillis.mohamed@gmail.com>
2016-09-16 15:41:26 +0100

    yan

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//22222222222222222
+		//222222222222222222222222222222222
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
